884cc6ca2684f9d2d81e7289a6488c5ee857f779
heyttor augusto <163946499+H3ytt0r@users.noreply.github.com>
2024-08-13 15:19:46 -0300

    Delete src/cadastros directory

--- src/cadastros/CampoEmBrancoException.java
+++
@@ -1,7 +0,0 @@
-package cadastros;
-
-public class CampoEmBrancoException extends Exception {
-    public CampoEmBrancoException(String campo) {
-        super("O campo '" + campo + "' não pode estar em branco.");
-    }
-}
